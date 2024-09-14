7b059dcd51e0da8998812196e67388596878d2df
Rory Pearson <Rorypearson2003@hotmail.com>
2024-09-14 13:57:17 +0100

    chore: Add setup script

---
+++ setup.sh
@@ -0,0 +1,36 @@
+# Check if Go works
+if ! command -v go &> /dev/null
+then
+    echo "go could not be found"
+    exit
+else
+    echo "[)(] Found Go"
+fi
+
+# Check if yarn works
+if ! command -v yarn &> /dev/null
+then
+    echo "yarn could not be found"
+    exit
+else
+    echo "[)(] Found Yarn"
+fi
+
+# Check if .env exists
+if [ ! -f .env ]; then
+    echo "Cannot find .env file"
+else
+    echo "[)(] Found .env"
+fi
+
+# Install UI dependencies in the ui folder
+cd ui
+echo "[)(] Installing UI dependencies"
+yarn install
+
+## Build the UI
+echo "[)(] Building UI"
+yarn build
+
+# Go back to the root folder
+cd ..
